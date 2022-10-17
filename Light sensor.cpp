e51e7542ef9b9ed622200e78fb79846195f5b8a9
luk37692 <115800238+luk37692@users.noreply.github.com>
2022-10-17 11:39:08 +0200

    Create Light sensors.cpp

---
+++ Light sensor.cpp
@@ -0,0 +1,23 @@
+ #include <Arduino.h>
+
+ 
+void setup()
+{
+  Serial.begin(9600); //Start serial monitor 
+}
+ 
+ 
+void loop()
+{
+  int value = analogRead(A0);  //Value get the light sensor values
+ 
+  printf("light: %d",value); // Print light sensor values
+  delay(100);
+}
+
+
+void LightSensor(){
+  int lux = analogRead(A0); //
+  return lux 
+}
+
