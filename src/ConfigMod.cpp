a55229a68cc2a9524c9c1a08dfb530592d1e0953
Thienpont Timothee <85194871+timo-thi@users.noreply.github.com>
2022-10-18 22:54:50 +0200

    Comments
    Added comments + some names changes

--- src/ConfigMod.cpp
+++ src/ConfigMod.cpp
@@ -7,19 +7,19 @@ void check_config()
     if (digitalRead(red_button) == HIGH) return; // Si jamais le bouton rouge n'est pas pressé
     led.setColorRGB(0, 55, 55, 0); // Set led color (0,R,G,B)
     start_serial();
-    collect_params.mode = 3;
-    initialisation_interruption_tim(1000000); //Définit le timer 1 à la valeur indiquée
+    collect_params.mode = 3; // Config mode number is 3.
+    initialisation_interruption_tim(1000000); // Définit le timer 1 à la valeur indiquée. (1000000 -> une occurence par sec)
     timer = configTimeout; //Initialiser le timer
-    while (timer > 0)
+    while (timer > 0) // While timeout isn't exceeded
     {
-        present();
+        wait_for_param();
     }
     stop_serial();
 }
 
 bool wait_for_entry(String *command, int *value){
-    while ((Serial.available() <= 0) && (timer >= 0)) {}
-    if (timer <= 0) 
+    while ((Serial.available() <= 0) && (timer >= 0)) {} // Wait for something to occur in serial buffer OR timeout
+    if (timer <= 0) // if the timout stopped the while, return true to quit config mode
     {
         return true;
     }
@@ -31,14 +31,14 @@ bool wait_for_entry(String *command, int *value){
         *command = command->substring(0, pos);
     }
     else {
-        command->trim();
+        command->trim(); // Remove every parasite end chracters.
     }
     return false;
 }
 
-void present()
+void wait_for_param()
 {
-    //Pose les paramètres par défaut
+    // Define default values for each parameter
     static int LOG_INTERVALL = 10;
     static int FILE_MAX_SIZE = 4096;
     static int TIMEOUT = 3000;
@@ -55,7 +55,7 @@ void present()
     static int PRESSURE_MIN = 850;
     static int PRESSURE_MAX = 1080;
 
-    //gestion d'une réponse pour la configuration
+    // Answer handeling for a command
     bool C = 0;
     if (Serial.available() > 0) //Permet de voir ce qui sera écrit
     {
@@ -67,117 +67,101 @@ void present()
         else Serial.println("Erreur "); //arrêt
     }
 
-    //Commence à modéliser une interraction avec l'utilisateur
+    // Start serial custom for user entry
     Serial.println("Entrez ce que vous souhaitez changer");
     Serial.println("------------------------------------");
     Serial.print("WWW_User:~$ ");
 
     String command;
     int value;
-    if (wait_for_entry(&command, &value)) return;
+    if (wait_for_entry(&command, &value)) return; // If true, return to exit config mode
 
-    Serial.print("Found command : ");
-    Serial.print(command);
-    Serial.print("=");
-    Serial.println(value);
+    Serial.print("Found command : "); // DEBUG
+    Serial.print(command);           // or
+    Serial.print("=");              // user interface
+    Serial.println(value);         // custom
 
+    // Check wich command is concerned, or, tell if not found
     if(command == String("LOG_INTERVALL"))
-    //Si la valeur entrée est 1 alors on modifie LOG_INTERVALL
     {
         LOG_INTERVALL = value;
         Serial.println(LOG_INTERVALL);
     }
     else if(command == String("FILE_MAX_SIZE"))
-    //Si la valeur entrée est 1 alors on modifie FILE_MAX_SIZE
     {
         FILE_MAX_SIZE = value;
         Serial.println(FILE_MAX_SIZE);
     }
     else if(command == String("TIMEOUT"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         TIMEOUT = value;
         Serial.println(TIMEOUT);
     }
     else if(command == String("LUMIN"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         LUMIN = value;
         Serial.println(LUMIN);
     }
     else if(command == String("LUMIN_LOW"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         LUMIN_LOW = value;
         Serial.println(LUMIN_LOW);
     }
     else if(command == String("LUMIN_HIGH"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         LUMIN_HIGH = value;
         Serial.println(LUMIN_HIGH);
     }
     else if(command == String("TEMP_AIR"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         TEMP_AIR = value;
         Serial.println(TEMP_AIR);
     }
     else if(command == String("MIN_TEMP_AIR"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         MIN_TEMP_AIR = value;
         Serial.println(MIN_TEMP_AIR);
     }
     else if(command == String("MAX_TEMP_AIR"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         MAX_TEMP_AIR = value;
         Serial.println(MAX_TEMP_AIR);
     }
     else if(command == String("HYGR"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         HYGR = value;
         Serial.println(HYGR);
     }
     else if(command == String("HYGR_MINT"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         HYGR_MINT = value;
         Serial.println(HYGR_MINT);
     }
     else if(command == String("HYGR_MAXT"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         HYGR_MAXT = value;
         Serial.println(HYGR_MAXT);
     }
     else if(command == String("PRESSURE"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         PRESSURE = value;
         Serial.println(PRESSURE);
     }
     else if(command == String("PRESSURE_MAX"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         PRESSURE_MAX = value;
         Serial.println(PRESSURE_MAX);
     }
     else if(command == String("PRESSURE_MIN"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         PRESSURE_MIN = value;
         Serial.println(PRESSURE_MIN);
     }
     else if(command == "RESET")
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         Serial.println("Reseting");
     }
     else if(command == String("VERSION"))
-    //Si la valeur entrée est 1 alors on modifie TIMEOUT
     {
         Serial.println("Version and serial number");
     }
