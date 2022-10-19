21635513a9c7cbf7887e06b9ddeeb8e6febef421
Thienpont Timothee <85194871+timo-thi@users.noreply.github.com>
2022-10-20 00:15:13 +0200

    Started kind of loop structure

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,35 @@
 Collect_params collect_params;
 
 void setup() {
-  // put your setup code here, to run once:
-  // start_serial();
-  init_rgb_led();
-  init_buttons();
-  check_config();
-  led.setColorRGB(0, 0, 255, 0);
-  initialisation_interruption_tim(1000);
+	// put your setup code here, to run once:
+	// start_serial();
+	init_rgb_led();
+	init_buttons();
+	check_config();
+	led.setColorRGB(0, 0, 255, 0);
+	initialisation_interruption_tim(1000);
 }
 
 void loop()
 {
-  // put your main code here, to run repeatedly:
+	// put your main code here, to run repeatedly:
+	switch (collect_params.mode)
+	{
+		case 1:
+			collect_params.eco_alternate_gps = !collect_params.eco_alternate_gps; // Something else different compring to std mode ?
+			break; // remove if we want to combine std and eco modes ?
+		case 0:
+			// fetch sensor data
+			// check consistency
+			// store in sd card
+			break;
+
+		case 2:
+			// fetch sensor data
+			// print data
+			break;
+		
+		default:
+			break;
+	}
 }
