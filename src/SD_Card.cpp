8753120f238c50d2def95e169f003ab813bbb67c
Thienpont Timothee <85194871+timo-thi@users.noreply.github.com>
2022-10-21 10:17:16 +0200

    safety commit

--- src/SD_Card.cpp
+++ src/SD_Card.cpp
@@ -12,7 +12,7 @@ void Write_SD_Card(int lumin, float hygr, float pressure, float temp)
     }
     
     Serial.println("sd");
-	File myFile = SD.open("TEST.log", FILE_WRITE); // SD.open(rtc.now().toString("AAMMDD_0.log"), FILE_WRITE);
+	File myFile = SD.open("0987.log", FILE_WRITE); // SD.open(rtc.now().toString("AAMMDD_0.log"), FILE_WRITE);
     // String donnee = rtc.now().toString("DD AA MM hh mm ss") + String(lumin) + String(hygr) + String(pressure) + String(temp);
     myFile.print(actTime());
     myFile.print(";");
